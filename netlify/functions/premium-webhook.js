4c19f56d10e18b6e535fc75b84127ef45024062e
pagamentospablorodriguez-dev <pagamentos.pablorodriguez@gmail.com>
2025-10-07 12:54:07 -0300

    Update premium-webhook.js

--- netlify/functions/premium-webhook.js
+++ netlify/functions/premium-webhook.js
@@ -1,5 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 
 // Inicializar Supabase Admin Client
 const supabaseAdmin = createClient(
